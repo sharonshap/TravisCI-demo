aad440a3d787ec4c723a0eac8709f0b8c80c7b06
sharonshap <sharon.shapira@gmail.com>
2017-01-30 14:50:15 +0200

    Update CalculatorSpec.js

--- tests/CalculatorSpec.js
+++ tests/CalculatorSpec.js
@@ -35,7 +35,7 @@ describe( 'calculator', function () {
       } );
   }
 
-  describe( 'Failures Test#' + i,
+  /*describe( 'Failures Test#' + i,
     function () {
       it( 'should add ' + number1 + ' plus ' + number2 + ' and be equals to ' + ( number1 + number2 ), function () {
         expect( Calculator.add( number1, number2 ) ).toBe( number1 - number2 );
@@ -60,5 +60,5 @@ describe( 'calculator', function () {
       it( 'should divide ' + number1 + '/' + number2 + ' and be equals to ' + ( number2 && number1 / number2 ), function () {
         expect( Calculator.div( number1, number2 ) ).toBe( number2 && number1 * number2 );
       } );
-    } );
+    } );*/
 } );
